d2876f3c70b0ac0e9bfdadd5da933332b8921f25
Claudio Neves <cfneguacu@gmail.com>
2026-01-19 21:46:08 -0300

    Trabalhando na Camada de UI

--- src/main/java/br/com/dio/persistence/dao/BoardColumnDAO.java
+++ src/main/java/br/com/dio/persistence/dao/BoardColumnDAO.java
@@ -1,12 +1,16 @@
 package br.com.dio.persistence.dao;
 
 import br.com.dio.persistence.entity.BoardColumnEntity;
+import br.com.dio.persistence.entity.BoardColumnKindEnum;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+
+import static br.com.dio.persistence.entity.BoardColumnKindEnum.findByName;
 
 @RequiredArgsConstructor
 public class BoardColumnDAO {
@@ -14,7 +18,7 @@ public class BoardColumnDAO {
     private final Connection connection;
 
     public BoardColumnEntity insert(final BoardColumnEntity entity) throws SQLException {
-        var sql = "INSERT INTO BOARDS_COLUMNS(name,"+"order"+",kind, board_id) values(?,?,?,?);";
+        var sql = "INSERT INTO BOARDS_COLUMNS(name,\"order\",kind, board_id) values(?,?,?,?);";
         try(var statement = connection.prepareStatement(sql)){
             var i = 1;
             statement.setString(i++, entity.getName());
@@ -27,6 +31,25 @@ public class BoardColumnDAO {
     }
 
     public List<BoardColumnEntity> findByBoardId(Long id) throws SQLException {
-        return null;
+
+        List<BoardColumnEntity> entities = new ArrayList<>();
+        var sql = "SELECT id , name, \"order\" FROM BOARDS_COLUMNS WHERE board_id = ? ORDER BY \"order\"";
+        try(var statement = connection.prepareStatement(sql)){
+
+            statement.setLong(1,id);
+            statement.executeQuery();
+            var resultSet = statement.getResultSet();
+            while(resultSet.next()){
+                var entity = new BoardColumnEntity();
+                entity.setId(resultSet.getLong("id"));
+                entity.setName(resultSet.getString("name"));
+                entity.setOrder(resultSet.getInt("order"));
+                entity.setKind(findByName(resultSet.getString("kind")));
+                entities.add(entity);
+            }
+            return entities;
+
+        }
+
     }
 }
